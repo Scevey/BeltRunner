75d848999df025708d2a530ad937db8bb1dd1508
narrill <miketeixeira95@gmail.com>
2016-04-18 20:59:17 -0400

    Fixing Vehicle

--- RoadRage/Vehicle.h
+++ RoadRage/Vehicle.h
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-/*----------------------------------------------
-Programmer: Alberto Bobadilla (labigm@gmail.com)
-Date: 2015/09
-----------------------------------------------*/
-#ifndef __APPLICATION_H_
-#define __APPLICATION_H_
-#pragma once
-
-#include "RE\ReEngAppClass.h"
-#include <SFML\Graphics.hpp>
-#include "MyBoundingSphereClass.h"
-
-using namespace ReEng; //Using ReEng namespace to use all the classes in the dll
-
-class Vehicle
-{
-	vector3 position; //position of vehicle on screen
-	int speed; //do we need speed?
-	int velocity; //change to float or double?
-
-	int getSpeed(); //get speed
-	int getVelocity(); //get velocity
-	
-	int accelerate(int vel); //take velocity and accelerate, return adjusted velocity
-	void move(int vel); //take velocity and move vehicle
-	void brake(); //slow down car or stop it completely?
-
-public:
-	Vehicle(); //constructor
-	~Vehicle(void); //destructor
-};
-
-#endif //__APPLICATION_H_
-=======
 // Paul Lampanaro
 // Vehicle Class will serve as parent for Truck & Car classes
 
@@ -154,4 +119,3 @@ private:
 };
 
 #endif //__VEHICLE_H__
->>>>>>> origin/master
