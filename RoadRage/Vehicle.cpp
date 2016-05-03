945fa5094364d4a87d3ae127fb604c9fd90e8372
Justin John <jxj7701@rit.edu>
2016-05-02 21:08:19 -0400

    More vehicle rotation work
    tweaked every value under the sun, fixed wobbling, made transitions
    smoother

--- RoadRage/Vehicle.cpp
+++ RoadRage/Vehicle.cpp
@@ -4,20 +4,34 @@
 void Vehicle::HandleRotation(void)
 {
 	// apply rotation acceleration to velocity
-	rotationAccel = -(velocity.x * 0.01f);
+	rotationAccel = -(velocity.x * 0.04f);
 
+	float retunVal;
+
+	if (isTurning == false) {
+		retunVal = 0.16f;
+		if (rotation < 1.0f && rotation > -1.0f) {
+			rotation = 0.0f;
+			rotationAccel = 0.0f;
+			rotationVel = 0.0f;
+		}
+	}
+	else {
+		retunVal = 0.08f;
+	}
 
 	// if not turning, realign
-	if (isTurning)
+	//if (isTurning)
+	if (true)
 	{
 		//positive banana
-		if (rotation <= 90.0f && rotation > 45.0f)
+		if (rotation <= 30.0f && rotation > 15.0f)
 		{
-			rotationVel -= 0.16f; // right now hardcoded
+			rotationVel -= 0.32f; // right now hardcoded
 		}
-		if (rotation <= 45.0f && rotation >=  0.0f)
+		if (rotation <= 15.0f && rotation >=  0.0f)
 		{
-			rotationVel -= 0.08f; // right now hardcoded
+			rotationVel -= retunVal; // right now hardcoded
 		}
 		/*
 		if (rotation < 0.0f && rotation > 0.0f)
@@ -26,13 +40,13 @@ void Vehicle::HandleRotation(void)
 		}*/
 
 		//negative banana
-		if (rotation >= -90.0f && rotation < -45.0f)
+		if (rotation >= -30.0f && rotation < -15.0f)
 		{
-			rotationVel += 0.16f; // right now hardcoded
+			rotationVel += 0.32f; // right now hardcoded
 		}
-		if (rotation >= -45.0f && rotation <= 0.0f)
+		if (rotation >= -15.0f && rotation <= 0.0f)
 		{
-			rotationVel += 0.08f; // right now hardcoded
+			rotationVel += retunVal; // right now hardcoded
 		}
 		/*
 		else if (rotation > 0.0f)
@@ -42,28 +56,28 @@ void Vehicle::HandleRotation(void)
 	}
 
 
-	if (isTurning = false)
-	{
-		//positive banana
-		if (rotation <= 90.0f && rotation > 45.0f){ rotationVel -= 0.16f; }
-		if (rotation <= 45.0f && rotation >= 0.0f){ 
-			rotationVel -= 0.08f; 
-			if (rotation == 0.0f) {
-				rotationAccel = 0.0f;
-				rotationVel = 0.0f;
-			}
-		}
+	//if (isTurning = false)
+	//{
+	//	//positive banana
+	//	if (rotation <= 90.0f && rotation > 45.0f){ rotationVel -= 0.16f; }
+	//	if (rotation <= 45.0f && rotation >= 0.0f){ 
+	//		rotationVel -= 0.08f; 
+	//		if (rotation == 0.0f) {
+	//			rotationAccel = 0.0f;
+	//			rotationVel = 0.0f;
+	//		}
+	//	}
 
-		//negative banana
-		if (rotation >= -90.0f && rotation < -45.0f){ rotationVel += 0.16f; }
-		if (rotation >= -45.0f && rotation <= 0.0f){ 
-			rotationVel += 0.08f; 
-			if (rotation == 0.0f) {
-				rotationAccel = 0.0f;
-				rotationVel = 0.0f;
-			}
-		}
-	}
+	//	//negative banana
+	//	if (rotation >= -90.0f && rotation < -45.0f){ rotationVel += 0.16f; }
+	//	if (rotation >= -45.0f && rotation <= 0.0f){ 
+	//		rotationVel += 0.08f; 
+	//		if (rotation == 0.0f) {
+	//			rotationAccel = 0.0f;
+	//			rotationVel = 0.0f;
+	//		}
+	//	}
+	//}
 
 	rotationVel += rotationAccel;
 
@@ -75,11 +89,11 @@ void Vehicle::HandleRotation(void)
 
 
 	//banana
-	if (rotation > 90.0f) {
-		rotation = 90.0f;
+	if (rotation > 30.0f) {
+		rotation = 30.0f;
 	}
-	if (rotation < -90.0f) {
-		rotation = -90.0f;
+	if (rotation < -30.0f) {
+		rotation = -30.0f;
 	}
 
 
